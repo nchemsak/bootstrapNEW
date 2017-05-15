8cf93f6feec94dd6e2547401aa1349d3386ff58f
Nicholas Chemsak <Nicholas Chemsak>
2017-05-15 07:47:44 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+console.log('Node is installed!');
+
+function add(a, b) {
+    var c = a + b;
+    return c;
+}
+
+
+console.log("add(5, 18)", add(5, 18));
+
+
+function subtract(a, b) {
+    var c = a - b;
+    return c;
+}
+
+console.log("i");
+console.log("subtract(5, 3)", subtract(5, 36));
+console.log("add(12, 3)", add(12, 3));
+console.log("add(100, 122)", add(100, 122));
+console.log("subtract(100, 12)", subtract(100, 12));
+console.log("subtract(100, 12)", subtract(100, 12));
